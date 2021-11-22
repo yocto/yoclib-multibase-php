3d81de9607c34547cafff9a760830c00f08076a9
Ben <ben@yocto.nu>
2021-11-22 12:42:58 +0100

    Fix base10 and base36

--- src/Base10.php
+++ src/Base10.php
@@ -8,12 +8,25 @@ class Base10{
         while(strlen($hex)%2!==0){
             $hex = '0'.$hex;
         }
+        foreach(str_split($decimal) AS $chunk){
+            if($chunk!=="0"){
+                break;
+            }
+            $hex = '00'.$hex;
+        }
         return hex2bin($hex);
     }
 
     public static function encode(string $data): string{
         $hex = bin2hex($data);
-        return BaseUtil::str_baseconvert($hex,16,10);
+        $decimal = BaseUtil::str_baseconvert($hex,16,10);
+        foreach(str_split($data) AS $chunk){
+            if($chunk!=="\0"){
+                break;
+            }
+            $decimal = '0'.$decimal;
+        }
+        return $decimal;
     }
 
 }
--- src/Base36.php
+++ src/Base36.php
@@ -3,14 +3,28 @@ namespace YOCLIB\Multiformats\Multibase;
 
 class Base36{
 
-    public static function decode(string $binary): string{
-        $hex = BaseUtil::str_baseconvert(strtolower($binary),36,16);
+    public static function decode(string $base36): string{
+        $hex = BaseUtil::str_baseconvert(strtolower($base36),36,16);
+        foreach(str_split($base36) AS $chunk){
+            if($chunk!=="0"){
+                break;
+            }
+            $hex = '00'.$hex;
+        }
         return hex2bin($hex);
     }
 
     public static function encode(string $data): string{
         $hex = bin2hex($data);
-        return BaseUtil::str_baseconvert($hex,16,36);
+        $base36 = BaseUtil::str_baseconvert($hex,16,36);
+        foreach(str_split($data) AS $chunk){
+            if($chunk!=="\0"){
+                break;
+            }
+            $base36 = '0'.$base36;
+        }
+
+        return $base36;
     }
 
 }
